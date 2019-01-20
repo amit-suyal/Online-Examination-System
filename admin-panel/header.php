1f2f72f8f14c8497f48f0020917a5ab5113042bb
Amit Suyal <amitsuyal175@gmail.com>
2019-01-20 12:23:01 -0800

    Update header.php

--- admin-panel/header.php
+++ admin-panel/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head><meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>usa</title>
+	<title>Admin-panel</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
